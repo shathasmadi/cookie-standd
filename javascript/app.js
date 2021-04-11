1308a320ee326707a824ab64113aad6274712804
shathasmadi <alsmadishatha@gmail.com>
2021-04-11 17:48:08 +0300

    add the second location

--- javascript/app.js
+++ javascript/app.js
@@ -54,3 +54,59 @@ for(let i =0 ; i<15 ; i++){
     list.textContent=`total: ${total}`;
   }
 }
+
+//tokyo
+const tokyo = {
+    minCust:3,
+    maxCust:24,
+    avgCookie:1.2,
+    avgCustPerHr:[],
+    cookPurchEachHr:[],
+    rand:function () {
+      return Math.random() * (this.maxCust - this.minCust) + this.minCust;
+    },
+    avgCust: function(){
+      for(let i=0 ; i < 14 ; i++){
+        this.avgCustPerHr[i]=this.rand();
+        this.cookPurchEachHr[i]=Math.floor(this.avgCustPerHr[i]*this.avgCookie);
+      }
+    },
+  
+  };
+  tokyo.avgCust();
+  
+  container=document.getElementById('cont');
+  locationName=document.createElement('h1');
+  container.appendChild(locationName);
+  locationName.textContent='Tokyo';
+  
+  
+  unorderedList=document.createElement('ul');
+  container.appendChild(unorderedList);
+  
+  amPm='';
+  list='';
+  total=0;
+  clock=5;
+  
+  
+  for(let i =0 ; i<15 ; i++){
+    list=document.createElement('li');
+    unorderedList.appendChild(list);
+    if (clock < 12 ){
+      clock++;
+    }else{
+      clock=1;
+    }if(clock < 12 && i<6){
+      amPm='am';
+    }
+    if(i>=6){
+      amPm='pm';
+    }
+    if(i<14){
+      list.textContent=`${clock} ${amPm}: ${tokyo.cookPurchEachHr[i]}`;
+      total=total+tokyo.cookPurchEachHr[i];
+    }else{
+      list.textContent=`total: ${total}`;
+    }
+  }
